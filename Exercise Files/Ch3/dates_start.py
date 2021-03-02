8e315afaf3d173e42748cb1fb47f3b0365125bcf
Alejandro Calzadilla <alejandrocalzadilla@gmail.com>
2021-03-02 16:07:19 -0500

    working with datetime

--- Exercise Files/Ch3/dates_start.py
+++ Exercise Files/Ch3/dates_start.py
@@ -1,26 +1,32 @@
 #
 # Example file for working with date information
 #
+from datetime import date
+from datetime import time
+from datetime import datetime
 
 
 def main():
   ## DATE OBJECTS
   # Get today's date from the simple today() method from the date class
-
+  today = date.today()
+  print ("today's date is : ", today)
 
   # print out the date's individual components
-
+  print("date components: ", today.day, today.month, today.year)
   
   # retrieve today's weekday (0=Monday, 6=Sunday)
-
-  
+  print("todays weekday number ", today.weekday())
+  days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"]
+  print(days[today.weekday()])
   ## DATETIME OBJECTS
   # Get today's date from the datetime class
-
+  today = datetime.now()
+  print(today)
   
   # Get the current time
-
- 
+  t = datetime.time(datetime.now())
+  print(t)
 
   
 if __name__ == "__main__":
